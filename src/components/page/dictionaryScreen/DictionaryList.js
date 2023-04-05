9e80f790d7dc161ad0a0f96b0ff552e38a6b7a75
mak.skid <skid12261@gmail.com>
2023-04-05 23:06:25 +0900

    Fixed the overwrapping issue of card and admob.

--- src/components/page/dictionaryScreen/DictionaryList.js
+++ src/components/page/dictionaryScreen/DictionaryList.js
@@ -1,10 +1,10 @@
 export const DictionaryList =  
     [
         { label: 'English-Japanese (EJDict)', value: 'EJ', id: 0},
-        /*{ label: 'American English', value: 'en_US', id: 1},
-        { label: 'British English', value: 'en_GB', id: 2},
-        { label: 'Français (French)', value: 'fr', id: 3 },
-        { label: 'Español (Spanish)', value: 'es', id: 4},
+        //{ label: 'American English', value: 'en_US', id: 1},
+        //{ label: 'British English', value: 'en_GB', id: 2},
+        //{ label: 'Français (French)', value: 'fr', id: 2 },
+        /*{ label: 'Español (Spanish)', value: 'es', id: 4},
         { label: 'Deutsch (German)', value: 'de', id: 5},
         { label: 'Italiano (Italian)', value: 'it', id: 6},
         { label: '日本語 (Japanese)', value: 'ja', id: 7},
