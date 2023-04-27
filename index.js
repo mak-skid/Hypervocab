d55e1afec549505cfee73b314eebcfc883905c2b
mak.skid <skid12261@gmail.com>
2023-04-27 21:32:52 +0900

    fully removed admob from android

--- index.js
+++ index.js
@@ -6,31 +6,5 @@ import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import mobileAds, { MaxAdContentRating } from 'react-native-google-mobile-ads';
-
-mobileAds()
-  .setRequestConfiguration({
-    // Update all future requests suitable for parental guidance
-    maxAdContentRating: MaxAdContentRating.PG,
-
-    // Indicates that you want your content treated as child-directed for purposes of COPPA.
-    tagForChildDirectedTreatment: true,
-
-    // Indicates that you want the ad request to be handled in a
-    // manner suitable for users under the age of consent.
-    tagForUnderAgeOfConsent: true,
-
-    // An array of test device IDs to allow.
-    testDeviceIdentifiers: ['EMULATOR'],
-  })
-  .then(() => {
-    console.log('Admob Request config successfully set!')
-  });
-
-mobileAds()
-    .initialize()
-    .then(adapterStatuses => {
-        console.log('Initialization complete!')
-    });
 
 AppRegistry.registerComponent(appName, () => App);
